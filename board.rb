6568c5d5e7f8ce3c0dfa971edaa45a5d3d65c812
Goncalo Pereira <goncalo.pereira@7digital.com>
2011-12-01 13:26:13 +0000

    moved routes to folder

--- board.rb
+++ board.rb
@@ -6,11 +6,7 @@ require 'faster_csv'
 require 'json'
 require 'mongo'
 
-require './environments.rb'
-require './applications.rb'
-require './statuses.rb'
-require './events.rb'
-require './services.rb'
+Dir['./routes/*.rb']. each { |file| require file } 
 
 configure do
 	set :db, Mongo::Connection.new.db("mydb")
@@ -19,22 +15,8 @@ configure do
 	set :show_exceptions, true
 end
 
-def get_request url, host_header
-	uri = URI(url)
-	http = Net::HTTP.new(uri.host, uri.port)
-	path = uri.path.empty? ? "/" : uri.path
-	query = uri.query.nil? ?  "" : "?" + uri.query	
-	headers =  {"Host"  => host_header }
-	puts "curl -v \"http://#{uri.host}#{path}#{query}\" -H \"Host: #{host_header}\""
-	return http.get(path + query, headers)
-end
-
 def error message
 	content_type :json
 	error = { :message => message}
 	error.to_json
 end
-
-
-
-
